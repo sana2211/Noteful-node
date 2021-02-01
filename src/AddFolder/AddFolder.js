beebb10df97f9e5e6dba6216870ec51fc48bfb48
Sana Siddiqui <sanafaizsiddiqui@me.com>
2021-02-01 21:54:11 +0400

    msg

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react'
 import NotefulForm from '../NotefulForm/NotefulForm'
 import './AddFolder.css'
+import conf from "../config";
 
 export default class AddFolder extends Component {
+  state = {
+    name: ""
+  }
+  handleInput = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      name: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const data = JSON.stringify(
+      {
+        folder_name: this.state.name
+      });
+    console.log(data);
+    fetch(conf.API_ENDPOINT+"/folders", {
+      method: "POST",
+      body: data,
+      headers: {"Content-Type": "application/json"}
+    })
+    .then(response=>response.json())
+    .then(response=>
+      {
+        if(response)
+        {
+          alert("Folder created!")
+        }
+      })
+    .catch(error=>console.log(error));
+  }
+  
   render() {
     return (
       <section className='AddFolder'>
         <h2>Create a folder</h2>
-        <NotefulForm>
+        <NotefulForm handleSubmit={this.handleSubmit}>
           <div className='field'>
             <label htmlFor='folder-name-input'>
               Name
             </label>
-            <input type='text' id='folder-name-input' />
+            <input type='text' id='folder-name-input' onChange={(e) => this.handleInput(e)} />
           </div>
           <div className='buttons'>
             <button type='submit'>
--- src/NotefulForm/NotefulForm.js
+++ src/NotefulForm/NotefulForm.js
@@ -2,9 +2,10 @@ import React from 'react'
 import './NotefulForm.css'
 
 export default function NotefulForm(props) {
+  console.log(props);
   const { className, ...otherProps } = props
   return (
-    <form
+    <form onSubmit={(e) => props.handleSubmit(e)}
       className={['Noteful-form', className].join(' ')}
       action='#'
       {...otherProps}
